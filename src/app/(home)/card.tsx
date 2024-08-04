f1223f75685f626aa58afe84fec2e887385f4134
Denner <dennergazevedo@gmail.com>
2024-08-04 20:56:40 -0300

    feat: get started card added

---
+++ src/app/(home)/card.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link"
+import { ElementType } from "react"
+
+interface HomeCardProps{
+  icon: ElementType
+  href: string
+  label: string
+  description: string
+}
+
+export default function HomeCard({href, icon: Icon, label, description}: HomeCardProps) {
+  return (
+    <Link href={href} className="flex flex-col pointer w-60 p-8 items-start justify-start gap-4 rounded border border-zinc-500 hover:bg-zinc-800 group">
+      <div className="flex flex-row gap-4 items-center">
+        <Icon className="h-8 w-8 flex-shrink-0 text-zinc-500 group-hover:text-sky-500"/>
+        <span className="text-slate-300 font-bold group-hover:text-sky-200">{label}</span>
+      </div>
+      <p className="text-xs text-zinc-500 leading-4 group-hover:text-white">{description}</p>
+    </Link>
+  )
+}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,14 @@
-import { Fragment } from 'react'
+import { Fragment } from "react";
+import HomeCard from "./card";
+
+import {
+  FileCode2,
+  Ampersands,
+  AlarmClock,
+  ListChecks,
+  Layers,
+  Code2,
+} from "lucide-react";
 
 export default function Home() {
   return (
@@ -13,11 +23,50 @@ export default function Home() {
               Welcome!
             </h2>
             <span className="text-sm text-zinc-500 dark:text-zinc-400">
-              Here you will find some useful tools for your daily life as a Developer.
+              Here you will find some useful tools for your daily life as a
+              Developer.
             </span>
           </div>
         </div>
+        <div className="mt-20 flex flex-row flex-wrap gap-4">
+          <HomeCard
+            icon={ListChecks}
+            label="To Do"
+            href="/"
+            description='A "To Do" is a list or task management tool used to organize and keep track of tasks or activities that need to be completed. '
+          />
+          <HomeCard
+            icon={AlarmClock}
+            label="Timer"
+            href="/"
+            description="A timer is a device or tool that measures and counts down a specific amount of time"
+          />
+          <HomeCard
+            icon={Code2}
+            label="Typescript"
+            href="/typescript"
+            description="A JSON to TypeScript interface converter is a tool that automatically generates TypeScript interface definitions from JSON data."
+          />
+          <HomeCard
+            icon={Layers}
+            label="CSV • JSON"
+            href="/csv"
+            description="A CSV to JSON and JSON to CSV converter is a tool that transforms data between CSV (Comma-Separated Values) and JSON (JavaScript Object Notation) formats."
+          />
+          <HomeCard
+            icon={Ampersands}
+            label="Base64"
+            href="/base64"
+            description="A Base64 encoder and decoder is a tool that converts data to and from Base64 encoding. "
+          />
+          <HomeCard
+            icon={FileCode2}
+            label="URI"
+            href="/uri"
+            description="A URI encoder and decoder is a tool that converts characters in a Uniform Resource Identifier (URI) into a format that can be transmitted over the internet."
+          />
+        </div>
       </div>
     </Fragment>
-  )
+  );
 }
